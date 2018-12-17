b1e8a1ab83f41928328d6987ba5224a1ff4355df
s18014 <s18014@std.it-college.ac.jp>
2018-12-18 01:31:17 +0900

    弾ヒット時のエフェクトを作成

--- Assets/Scripts/PlayerBullet/PlayerBulletCollider.cs
+++ Assets/Scripts/PlayerBullet/PlayerBulletCollider.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBulletCollider : MonoBehaviour {
+    [SerializeField] GameObject SparkPrefab;
+    private GameObject effectGroup;
 
-	// Use this for initialization
-	void Start () {
+    private void Awake() {
+        effectGroup = GameObject.Find("SparkEffectGroup");
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -16,6 +22,13 @@ public class PlayerBulletCollider : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Enemy") {
+            if (effectGroup.transform.childCount < 20) {
+                Vector2 hitPos = collision.bounds.ClosestPoint(transform.position);
+                hitPos.x *= Random.Range(0.9f, 1.1f);
+                hitPos.y *= Random.Range(0.9f, 1.1f);
+                var obj = (GameObject)Instantiate(SparkPrefab, hitPos, Quaternion.identity);
+                obj.transform.parent = effectGroup.transform;
+            }
             Destroy(gameObject);
         }
     }
